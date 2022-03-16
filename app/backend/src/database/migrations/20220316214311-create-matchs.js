f0746e3b1759f3d9fd86d844e78a7862af27086b
Williamns <williamnstolb@gmail.com>
2022-03-16 18:48:37 -0300

    Requisitos 1 e 2: ok (eu acho)

---
+++ app/backend/src/database/migrations/20220316214311-create-matchs.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matchs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        references: { model: 'Clubs', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      home_team_goals: {
+        type: Sequelize.NUMBER,
+        allowNull: false
+      },
+      away_team: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        references: { model: 'Clubs', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      away_team_goals: {
+        type: Sequelize.NUMBER,
+        allowNull: false
+      },
+      inProgress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matchs');
+  }
+};
