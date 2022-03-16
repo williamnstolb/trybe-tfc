2048cb15241facb796cc91c7c087f90b4b2882cb
Williamns <williamnstolb@gmail.com>
2022-03-16 18:24:42 -0300

    migration create-clubs ok

---
+++ app/backend/src/database/migrations/20220316212022-create-clubs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('clubs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      club_name: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('clubs');
+  }
+};
