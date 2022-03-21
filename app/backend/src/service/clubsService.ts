94f8c8bcd9a19367c54c2c56c27fde1c078fd3ca
Williamns <williamnstolb@gmail.com>
2022-03-21 17:34:12 -0300

    testando modificaçoes para ver se o front se conecta ao back

--- app/backend/src/service/clubsService.ts
+++ app/backend/src/service/clubsService.ts
@@ -7,10 +7,7 @@ async function getAllService() {
 }
 
 async function getByIdService(id: string) {
-  console.log('ID =====>', id);
   const club = await Club.findOne({ where: { id } });
-  console.log('CLUB =======>', club);
-
   return { message: club, status: StatusCode.OK };
 }
 
