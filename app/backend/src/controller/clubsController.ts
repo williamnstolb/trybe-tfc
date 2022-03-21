72e0e354f2b98390b8444e0a65aebcc286c52a20
Williamns <williamnstolb@gmail.com>
2022-03-21 16:02:16 -0300

    Requisito 17: Rota criada e conectada ao controller

--- app/backend/src/controller/clubsController.ts
+++ app/backend/src/controller/clubsController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
 import ResponseStatusMessage from '../interface/Response';
-import getAllService from '../service/clubsService';
+import { getAllService, getByIdService } from '../service/clubsService';
 
 async function getAll(req: Request, res: Response): Promise<void> {
   const response: ResponseStatusMessage = await getAllService();
   res.status(response.status).json(response.message);
 }
 
-export default getAll;
+async function getById(req: Request, res: Response): Promise<void> {
+  const { id } = req.params;
+  const response: ResponseStatusMessage = await getByIdService(id);
+  res.status(response.status).json(response.message);
+}
+
+export {
+  getAll,
+  getById,
+};
--- app/backend/src/routes/clubs.ts
+++ app/backend/src/routes/clubs.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
-import getAll from '../controller/clubsController';
+import { getAll, getById } from '../controller/clubsController';
 
 const route = express.Router();
 
 route.get('/', getAll);
+route.get('/:id', getById);
 
 export default route;
