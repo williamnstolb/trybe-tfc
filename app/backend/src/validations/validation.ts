4f6fad9de491e7ea45015c742b53104666672195
Williamns <williamnstolb@gmail.com>
2022-03-21 23:44:46 -0300

    Requisito 5: usando bcrypt para comparar senha

--- app/backend/src/validations/validation.ts
+++ app/backend/src/validations/validation.ts
@@ -1,22 +1,23 @@
-// import { compareSync } from 'bcryptjs';
+import { compareSync, hash } from 'bcryptjs';
 import User from '../database/models/User';
 import { IUser } from '../interface/User';
 
-export default async function passwordCorrect(email: string, password: string): Promise<boolean> {
-  const user: IUser | null = await User.findOne({ where: { email } });
-  if (user && password === user.password) {
-    return true;
-  }
-  return false;
-}
-
-// export default async function passawordCorrect(email: string, password: string): Promise<boolean> {
+// export default async function passwordCorrect(email: string, password: string): Promise<boolean> {
 //   const user: IUser | null = await User.findOne({ where: { email } });
-//   if (user) {
-//     return compareSync(password, user.password);
+//   if (user && password === user.password) {
+//     return true;
 //   }
 //   return false;
 // }
+
+export default async function passawordCorrect(email: string, password: string): Promise<boolean> {
+  const user: IUser | null = await User.findOne({ where: { email } });
+  if (user) {
+    const userPw = await hash(user.password, 10);
+    return compareSync(password, userPw);
+  }
+  return false;
+}
 
 // export default {
 //   passawordCorrect,
