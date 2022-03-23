35905d11736f262d08e6e29e3bdab0f4174a6c88
Williamns <williamnstolb@gmail.com>
2022-03-23 17:21:26 -0300

    Requisito 19: alteraçao na model Club

--- app/backend/src/database/models/Club.ts
+++ app/backend/src/database/models/Club.ts
@@ -1,7 +1,5 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
-import Match from './Match';
 
 class Club extends Model {
   // public <campo>!: <tipo>;
@@ -31,18 +29,5 @@ Club.init({
   tableName: 'clubs',
   timestamps: false,
 });
-
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-Club.hasMany(Match, { foreignKey: 'id', as: 'matchs', constraints: false });
 
 export default Club;
--- app/backend/src/service/matchsService.ts
+++ app/backend/src/service/matchsService.ts
@@ -1,8 +1,14 @@
 import Match from '../database/models/Match';
+import Club from '../database/models/Club';
 import StatusCode from '../utils/statusCode';
 
 async function getAllService() {
-  const response: Match[] = await Match.findAll();
+  const response: Match[] = await Match.findAll({
+    include: [
+      { model: Club, as: 'awayClub', attributes: ['clubName'] },
+      { model: Club, as: 'homeClub', attributes: ['clubName'] },
+    ],
+  });
   return { message: response, status: StatusCode.OK };
 }
 
