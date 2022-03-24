1dfd62970a96c72285b3b13f2ffe70779278b573
Williamns <williamnstolb@gmail.com>
2022-03-23 23:20:37 -0300

    Requisito 25: utils/message atualizada para passar no requisito

--- app/backend/src/utils/message.ts
+++ app/backend/src/utils/message.ts
@@ -3,7 +3,7 @@ enum Message {
   FIELD_MUST_BE_FILLED = 'All fields must be filled',
   UNAUTHORIZED = 'Unauthorized',
   MATCH_NOT_IN_PROGRESS = 'Match is not in progress',
-  SAME_TEAMS = 'Same teams',
+  SAME_TEAMS = 'It is not possible to create a match with two equal teams',
   MATCH_FINISHED = 'Match is finished',
 }
 
